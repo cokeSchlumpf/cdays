3810dc2510964f8a6634a1e62033d6a57598f07b
Dominik Gersak <dgersak@sk.ibm.com>
2018-06-07 15:11:17 +0200

    basic layout

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -6,32 +6,46 @@ import React, { Component } from 'react';
 import DoodyChat from '../DoodyChat';
 
 const FixedMenu = () => (
-  <Menu fixed='top' size='large'>
-    <Container>
-      <Menu.Item as='a' active>Home</Menu.Item>
-    </Container>
-  </Menu>);
+    <Menu fixed='top' size='large'>
+        <Container>
+            <Menu.Item as='a' active>Home</Menu.Item>
+        </Container>
+    </Menu>);
 
 class App extends Component {
-  state = {};
+    state = {};
 
-  hideFixedMenu = () => this.setState({ visible: false });
-  showFixedMenu = () => this.setState({ visible: true });
+    hideFixedMenu = () => this.setState({ visible: false });
+    showFixedMenu = () => this.setState({ visible: true });
 
-  onMessage = (message) => {
-    console.log(message);
-  }
+    onMessage = (message) => {
+        console.log(message);
+    }
 
-  render() {
-    const { visible } = this.state;
+    render () {
+        const { visible } = this.state;
 
-    return (
-      <div className="app-app">
-        <div className="app-header">
-          HEADER !!!!!!
-        </div>
-      </div>);
-  }
+        return (
+            <div className="app-app">
+                <div className="app-header">
+                    <div className="app-header-logo"></div>
+                </div>
+                <div className="app-cases-menu">
+                    Cases Menu
+                </div>
+                <div className="app-main-container">
+                    <div className="app-sidebar-left">
+                        left
+                    </div>
+                    <div className="app-main">
+                    </div>
+                    <div className="app-sidebar-right">
+                        right
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
