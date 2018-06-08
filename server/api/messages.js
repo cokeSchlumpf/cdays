40a4bd2b2d74735ec8f7c50d6a477a25014a1782
Dominik Gersak <dgersak@sk.ibm.com>
2018-06-08 09:55:28 +0200

    added hardcoded timeline items by ID

--- server/api/messages.js
+++ server/api/messages.js
@@ -10,85 +10,156 @@ const api = express.Router();
  * Retrieves the user information
  */
 api.post('/', (req, res) => {
-  const user_id = req.body.user_id;
-  const message = req.body.message;
+    const user_id = req.body.user_id;
+    const message = req.body.message;
 
-  store.dispatch(actions.client.chat.eventToClient({ message }));
-  res.sendStatus(200);
+    store.dispatch(actions.client.chat.eventToClient({ message }));
+    res.sendStatus(200);
 });
 
 api.post('/message', (req, res) => {
-  store.dispatch({
-    type: 'COMPONENTS_CHAT_BOT_MESSAGE',
-    broadcast: true,
-    payload: req.body
-  });
+    store.dispatch({
+        type: 'COMPONENTS_CHAT_BOT_MESSAGE',
+        broadcast: true,
+        payload: req.body
+    });
 
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 api.post('/add-item-andre', (req, res) => {
-  store.dispatch(actions.services.users.addTimelineentry('andre', req.body));
+    store.dispatch(actions.services.users.addTimelineentry('andre', req.body));
 
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 api.get('/', (req, res) => {
-  // store.dispatch(actions.client.chat.eventToClient({ message: 'TEST MESSAGE' }));
+    // store.dispatch(actions.client.chat.eventToClient({ message: 'TEST MESSAGE' }));
 
-  store.dispatch(actions.services.users.addTimelineentry('andre', {
-    type: 'letter',
-    subject: 'Eingang unterzeichneter Vertrag',
-    date: '2012-03-10',
-    agent: 'Sam Schaufelberg',
-    action: false,
-    labels: ['Motorradversicherung', 'Vertragseingang']
-  }));
 
-  res.sendStatus(200);
-});
+    const id = +req.param('id');
+    let msg;
+
+    switch (id) {
+        case 1:
+            msg = {
+                type: 'chat',
+                subject: 'Adresse geändert',
+                date: '2012-06-08',
+                agent: 'Virtual Agent',
+                action: false,
+                labels: ['Adressänderung']
+            };
+            break;
+        case 2:
+            msg = {
+                type: 'mail',
+                subject: 'Prüfung der zu versichernden Objekte',
+                date: '2012-06-08',
+                agent: 'Virtual Agent',
+                action: false,
+                labels: ['Deckungsprüfung', 'Hausratversicherung']
+            };
+            break;
+        case 3:
+            msg = {
+                type: 'mail',
+                subject: 'AW: Prüfung der zu versichernden Objekte',
+                date: '2012-06-08',
+                agent: '-',
+                action: false,
+                labels: ['Deckungsprüfung', 'Hausratversicherung']
+            };
+            break;
+        case 4:
+            msg = {
+                type: 'chat',
+                subject: 'Kundengespräch Hausrat Comfort',
+                date: '2012-06-08',
+                agent: 'Menderes, Katharina',
+                action: false,
+                labels: ['Anfrage', 'Hausratversicherung']
+            };
+            break;
+        case 5:
+            msg = {
+                type: 'mail',
+                subject: 'Angebot Hausrat Comfort',
+                date: '2012-06-08',
+                agent: 'Menderes, Katharina',
+                action: false,
+                labels: ['Angebot', 'Hausratversicherung']
+            };
+            break;
+        case 6:
+            msg = {
+                type: 'letter',
+                subject: 'Eingang Angebot Hausrat Comfort',
+                date: '2012-06-08',
+                agent: 'Menderes, Katharina',
+                action: false,
+                labels: ['Angebot', 'Hausratversicherung']
+            };
+            break;
+        default:
+            msg = {
+                type: 'letter',
+                subject: `Eingang unterzeichneter Vertrag`,
+                date: '2012-03-10',
+                agent: 'Sam Schaufelberg',
+                action: false,
+                labels: ['Motorradversicherung', 'Vertragseingang']
+            };
+            break;
+    }
+
+
+    store.dispatch(actions.services.users.addTimelineentry('andre', msg));
+
+    res.sendStatus(200);
+})
+;
 
 api.get('/user-message', (req, res) => {
-  store.dispatch({
-    type: 'COMPONENTS_CHAT_BOT_MESSAGE',
-    broadcast: true,
-    payload: {
-      message: 'Hello World!',
-      sender: 'Egon Olsen'
-    }
-  });
+    store.dispatch({
+        type: 'COMPONENTS_CHAT_BOT_MESSAGE',
+        broadcast: true,
+        payload: {
+            message: 'Hello World!',
+            sender: 'Egon Olsen'
+        }
+    });
 
-  store.dispatch({
-    type: 'COMPONENTS_CHAT_BOT_MESSAGE',
-    broadcast: true,
-    payload: {
-      message: 'Hallo lieber Andre!',
-      sender: 'user'
-    }
-  });
+    store.dispatch({
+        type: 'COMPONENTS_CHAT_BOT_MESSAGE',
+        broadcast: true,
+        payload: {
+            message: 'Hallo lieber Andre!',
+            sender: 'user'
+        }
+    });
 
-  store.dispatch({
-    type: 'COMPONENTS_CHAT_BOT_MESSAGE',
-    broadcast: true,
-    payload: {
-      message: 'Jo!',
-      sender: 'Egon Olsen'
-    }
-  });
+    store.dispatch({
+        type: 'COMPONENTS_CHAT_BOT_MESSAGE',
+        broadcast: true,
+        payload: {
+            message: 'Jo!',
+            sender: 'Egon Olsen'
+        }
+    });
 
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 api.get('/show-chat', (req, res) => {
-  store.dispatch({
-    type: 'COMPONENTS_CHAT_WINDOW_SHOW',
-    broadcast: true,
-    payload: {}
-  });
+    store.dispatch({
+        type: 'COMPONENTS_CHAT_WINDOW_SHOW',
+        broadcast: true,
+        payload: {}
+    });
 
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
-
 
 
 module.exports = api;
